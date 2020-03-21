88cf258c8a02c878aa31dd5e747c192a32d2f5ab
Hans Müller-Dieckert <Hans.Mueller-Dieckert@web.de>
2020-03-21 14:03:55 +0100

    Initial commit

---
+++ backend/src/main/java/de/wirvsvirus/dgsdoktorundinformation/DgsDoktorUndInformationApplication.java
@@ -0,0 +1,13 @@
+package de.wirvsvirus.dgsdoktorundinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsDoktorUndInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsDoktorUndInformationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/wirvsvirus/dgsdoktorundinformation/DgsDoktorUndInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wirvsvirus.dgsdoktorundinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsDoktorUndInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
