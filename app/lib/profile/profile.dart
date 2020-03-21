1f5604ebf4b525714f5acdd41b9780cfb3d2160a
Moritz Otto <moritz-otto@gmx.de>
2020-03-21 15:47:50 +0100

    Profilemodel angelegt

---
+++ app/lib/profile/profile.dart
@@ -0,0 +1,22 @@
+class Profile {
+  String name;
+  DateTime birthday;
+
+  String contactName;
+  String contactTelephone;
+
+  Profile() {}
+
+  Profile.fromJson(Map<String, dynamic> json) :
+        name = json["name"],
+        birthday = json["birthday"],
+        contactName = json["contactName"],
+        contactTelephone = json["contactTelephone"];
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "birthday": birthday,
+    "contactName": contactName,
+    "contactTelephone": contactTelephone
+  };
+}
