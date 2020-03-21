996b861953d10d99a9a131862f7290d2c20ff458
codeStev <codeStev@users.noreply.github.com>
2020-03-21 18:56:07 +0100

    MenuBar hinzugefuegt

---
+++ webapp/src/components/MenuBar.vue
@@ -0,0 +1,30 @@
+<template>
+
+
+    <v-app-bar
+            app
+            color="primary"
+            dark
+    >
+        <!--  TODO nerviger Button, ander Loesung finden   -->
+        <v-btn  text to="/Home"><v-toolbar-title >
+            DGSDocInfo
+        </v-toolbar-title>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+        <v-btn  text to="/Faq" >FAQ</v-btn>
+
+    </v-app-bar>
+
+</template>
+
+<script>
+    export default {
+        name: "MenuBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ webapp/src/views/Faq.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+
+        Hier kommt noch ein FAQ hin!
+
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Faq"
+    }
+</script>
+
+<style scoped>
+
+</style>
