5287f746d2cdd5a093e97d2b3a5b2d1f43428b3a
Matthias-Kuhn <62451083+Matthias-Kuhn@users.noreply.github.com>
2020-03-22 15:59:13 +0100

    Update colors.dart

--- app/lib/colors.dart
+++ app/lib/colors.dart
@@ -5,5 +5,7 @@ class ThemeColors {
   static const darkgrey = Color.fromRGBO(55, 65, 81, 100);
   static const appBar = Color.fromRGBO(235, 239, 243, 100);
   static const Icon    = Colors.white;
+  static const LightBlue = Color(0xffe1e5e9);
+  static const DarkBlue = Color(0xff374151);
 
 }
