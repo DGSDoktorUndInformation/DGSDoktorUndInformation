cf979d57e701c2aefac003112a6ca0ea8bae63fa
Moritz Otto <moritz-otto@gmx.de>
2020-03-22 11:51:20 +0100

    Id und Timestamp zur Message hinzugefügt

--- backend/src/main/java/de/wirvsvirus/dgsdoktorundinformation/message/Message.java
+++ backend/src/main/java/de/wirvsvirus/dgsdoktorundinformation/message/Message.java
@@ -1,28 +1,20 @@
 package de.wirvsvirus.dgsdoktorundinformation.message;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.UUID;
 
+import lombok.Data;
+
+@Data
 public class Message {
 
+	private UUID id;
 	private String nachrichtenText;
 	private String videoPfad;
+	private boolean fromPatient;
+	private LocalDateTime date;
 	private Collection<String> antwortOptionen = new ArrayList<>();
-	
-	public Collection<String> getAntwortOptionen() {
-		return antwortOptionen;
-	}
-	public String getVideoPfad() {
-		return videoPfad;
-	}
-	public void setVideoPfad(String videoPfad) {
-		this.videoPfad = videoPfad;
-	}
-	public String getNachrichtenText() {
-		return nachrichtenText;
-	}
-	public void setNachrichtenText(String nachrichtenText) {
-		this.nachrichtenText = nachrichtenText;
-	}
-	
+
 }
--- backend/src/main/java/de/wirvsvirus/dgsdoktorundinformation/session/SessionController.java
+++ backend/src/main/java/de/wirvsvirus/dgsdoktorundinformation/session/SessionController.java
@@ -1,29 +1,15 @@
 package de.wirvsvirus.dgsdoktorundinformation.session;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-import javax.websocket.server.PathParam;
-
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.*;
 
 import de.wirvsvirus.dgsdoktorundinformation.message.Message;
-import de.wirvsvirus.dgsdoktorundinformation.video.VideoController;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SessionController {
-	
+
 	private Map<String,Session> sessionStore = new HashMap<>();
 
 	@PostMapping("/session")
@@ -45,12 +31,12 @@ public class SessionController {
 		sessionResponse.add(WebMvcLinkBuilder.linkTo((WebMvcLinkBuilder.methodOn(SessionController.class)).loadSession(sessionResponse.getPatientenCode())).withSelfRel());
 		return sessionResponse;
 	}
-	
+
 	@GetMapping("/session/{patientenCode}")
 	@ResponseBody
 	public SessionResponse loadSession( @PathVariable String patientenCode) {
 		Session session = sessionStore.get(patientenCode);
-		
+
 		//  Load Session with SessionId
 		SessionResponse sessionResponse = new SessionResponse();
 		sessionResponse.setPatientenCode(patientenCode);
@@ -60,18 +46,21 @@ public class SessionController {
 		sessionResponse.add(WebMvcLinkBuilder.linkTo((WebMvcLinkBuilder.methodOn(SessionController.class)).loadSession(sessionResponse.getPatientenCode())).withSelfRel());
 		return sessionResponse;
 	}
-	
+
 	@PostMapping("/session/{patientenCode}/message")
 	public void postMessage(@RequestBody Message message, @PathVariable String patientenCode) {
+		message.setId(UUID.randomUUID());
+		message.setDate(LocalDateTime.now());
+
 		sessionStore.get(patientenCode).getMessages().add(message);
 		System.out.println(message.getVideoPfad());
 		System.out.println("Storing Message " + message.getNachrichtenText() + " to Session "+patientenCode);
 	}
-	
+
 	@GetMapping("/session/{patientenCode}/message")
 	public List<Message> getLastMessage(@PathVariable String patientenCode) {
 		List<Message> response = new ArrayList<>();
 		return sessionStore.get(patientenCode).getMessages();
 	}
-	
+
 }
