456083240471728da77314fa485700e9e3142cb7
Druidikaaa <ennoschwanke90@gmail.com>
2020-03-22 14:06:22 +0100

    EmbeddedVideo auch für News-Sektion

--- app/lib/news/newsPage.dart
+++ app/lib/news/newsPage.dart
@@ -6,7 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewsPage extends StatefulWidget {
-
   NewsPage();
 
   @override
@@ -16,25 +15,19 @@ class NewsPage extends StatefulWidget {
 }
 
 class NewsPageState extends State<NewsPage> {
-
   var test;
 
-  generateNews(String title,
-               Image thumbnail,
-               String url,
-               DateTime timestamp,
-               bool isVideo){
-
+  generateNews(String title, Image thumbnail, String url, DateTime timestamp,
+      bool isVideo, bool isEmbeddedVideo) {
     var testNews = new News();
-    testNews.title =title;
+    testNews.title = title;
     testNews.thumbnail = thumbnail;
-    testNews.url       = url;
-    testNews.isVideo   = isVideo;
+    testNews.url = url;
+    testNews.isVideo = isVideo;
     testNews.timestamp = timestamp;
+    testNews.isEmbeddedVideo = isEmbeddedVideo;
 
     test.add(testNews);
-
-
   }
 
   Future<List<News>> _getNewsData() async {
@@ -42,31 +35,50 @@ class NewsPageState extends State<NewsPage> {
 
     test = new List<News>();
 
-    generateNews("WDR COSMO Corono-News",
-                 Image.network("https://www1.wdr.de/funkhaus-europa-wird-cosmo-106~_v-TeaserAufmacher.jpg"),
-                 "https://www1.wdr.de/mediathek/video/radio/cosmo/video-corona-news-in-gebaerdensprache---104.html",
-    DateTime.now(),
-    true);
-
-    generateNews("Informationen in leichter Sprache",
-        Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/BMG_Logo.svg/2000px-BMG_Logo.svg.png"),
-        "https://www.bundesgesundheitsministerium.de/coronavirus/coronavirus-leichte-sprache.html?fbclid=iwar2n1ied0umao8zlzxa0bhgutr07x3n691ysidrutzq4jtgjhsbt9go9pce",
-        new DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day - 1),
+    generateNews(
+        "WDR COSMO Corono-News",
+        Image.network(
+            "https://www1.wdr.de/funkhaus-europa-wird-cosmo-106~_v-TeaserAufmacher.jpg"),
+        "https://www1.wdr.de/mediathek/video/radio/cosmo/video-corona-news-in-gebaerdensprache---104.html",
+        DateTime.now(),
+        true,
         false);
 
-    generateNews("Pressemitteilung Badenwürtenberg",
-        Image.network("https://i.vimeocdn.com/video/866311781.webp?mw=1000&mh=563&q=70"),
+    generateNews(
+        "Informationen in leichter Sprache",
+        Image.network(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/BMG_Logo.svg/2000px-BMG_Logo.svg.png"),
+        "https://www.bundesgesundheitsministerium.de/coronavirus/coronavirus-leichte-sprache.html?fbclid=iwar2n1ied0umao8zlzxa0bhgutr07x3n691ysidrutzq4jtgjhsbt9go9pce",
+        new DateTime(
+            DateTime.now().year, DateTime.now().month, DateTime.now().day - 1),
+        false,
+        false);
+
+    generateNews(
+        "Pressemitteilung Badenwürtenberg",
+        Image.network(
+            "https://i.vimeocdn.com/video/866311781.webp?mw=1000&mh=563&q=70"),
         "https://player.vimeo.com/video/398592777",
-        new DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day - 5),
+        new DateTime(
+            DateTime.now().year, DateTime.now().month, DateTime.now().day - 5),
+        true,
+        false);
+
+    generateNews(
+        "Pressemitteilung Badenwürtenberg",
+        Image.network(
+            "https://ksl-msi-nrw.de/public/styles/large/public/ksl/msi/bilder/Vorschau_Corona_Video.jpg?h=02882563&itok=5MAtx90k"),
+        "https://ksl-msi-nrw.de/public/ksl/msi/video/ksl_corona_virus_MAGS_UT.mp4",
+        new DateTime(
+            DateTime.now().year, DateTime.now().month, DateTime.now().day - 5),
+        true,
         true);
 
     return test;
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
       print("CREATE LIST VIEW");
       List<News> values = snapshot.data;
@@ -83,7 +95,6 @@ class NewsPageState extends State<NewsPage> {
       return new ListView.builder(
         itemCount: length,
         itemBuilder: (BuildContext context, int index) {
-
           return new Column(
             children: <Widget>[
               newsTile(values[index], context),
@@ -104,11 +115,9 @@ class NewsPageState extends State<NewsPage> {
           default:
             if (snapshot.hasError)
               return new Text('Error: ${snapshot.error}');
-            else
-            if(snapshot.data != null && snapshot.data.length != 0){
+            else if (snapshot.data != null && snapshot.data.length != 0) {
               return createListView(context, snapshot);
-            }
-            else{
+            } else {
               return new Text('KEINE NEWS!');
             }
         }
@@ -118,31 +127,25 @@ class NewsPageState extends State<NewsPage> {
     return Scaffold(
         backgroundColor: ThemeColors.Primary,
         appBar: AppBar(
-          backgroundColor: ThemeColors.Primary,
-          actions: <Widget>[
-          ],
+            backgroundColor: ThemeColors.Primary,
+            actions: <Widget>[],
 
+            //onPressed: () async => onSave(context),
 
-          //onPressed: () async => onSave(context),
-
-
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        title: AppBarContent(),
-        leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: Icon(
-              Icons.arrow_back,
-              color: ThemeColors.Icon,
-            ),
-            tooltip: 'Zurück')),body
-    :
-    SingleChildScrollView
-    (
-       child: Container(
-         height: MediaQuery.of(context).size.height - 80,
-         child: futureBuilder,
-       )
-    ));
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            title: AppBarContent(),
+            leading: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: ThemeColors.Icon,
+                ),
+                tooltip: 'Zurück')),
+        body: SingleChildScrollView(
+            child: Container(
+          height: MediaQuery.of(context).size.height - 80,
+          child: futureBuilder,
+        )));
   }
 }
