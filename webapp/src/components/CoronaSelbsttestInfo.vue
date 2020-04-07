9648a3c63a30f6f08be6f80036de67f5343c0180
codeStev <codeStev@users.noreply.github.com>
2020-04-07 23:27:43 +0200

    -Anzeige des Testdatums auf der Selbsttest-Kachel -Keine Anzeige des Datums in der Selbsttest-Ansicht

--- webapp/src/components/CoronaSelbsttestInfo.vue
+++ webapp/src/components/CoronaSelbsttestInfo.vue
@@ -1,23 +1,28 @@
 <!--TODO Daten einfuegen, wenn sie vom Backend bereitgestellt werden-->
 <template>
     <v-container>
-        <v-card  class="mx-auto"
-                 max-width="344"
+        <v-card class="mx-auto"
+                max-width="344"
         >
-            <v-row >
-                <v-col  >
-                            <v-card-title class="headline">Selbsttest</v-card-title>
-                            <v-card-actions>
-                                <v-dialog max-width="600px"  v-model="dialog" persistent  >
-                                   <template v-slot:activator="{ on }">
-                                       <v-btn primary v-on="on">
-                                           Ergebnisse einsehen
-                                       </v-btn>
-                                   </template>
-                                    <Selbsttest  @destroyModal="toogleDialog"  :data="data"></Selbsttest>
-                                </v-dialog>
+            <v-row>
+                <v-col>
+                    <v-list-item three-line>
+                        <v-list-item-content>
+                            <v-list-item-title class="headline mb-1">Selbsttest</v-list-item-title>
+                            <v-list-item-subtitle> Getestet am : {{testdatum}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-card-actions>
+                        <v-dialog max-width="600px" v-model="dialog" persistent>
+                            <template v-slot:activator="{ on }">
+                                <v-btn primary v-on="on">
+                                    Ergebnisse einsehen
+                                </v-btn>
+                            </template>
+                            <Selbsttest @destroyModal="toogleDialog" :data="data"></Selbsttest>
+                        </v-dialog>
 
-                            </v-card-actions>
+                    </v-card-actions>
                 </v-col>
             </v-row>
         </v-card>
@@ -27,11 +32,12 @@
 
 <script>
     import Selbsttest from "./Selbsttest";
+    import moment from "moment";
 
     export default {
         name: "CoronaSelbsttestInfo",
         components: {Selbsttest},
-        data () {
+        data() {
             return {
                 dialog: false,
             }
@@ -42,14 +48,22 @@
                 required: true
             }
         },
-        methods:{
-            toogleDialog(){
-                if(this.dialog===false){
-                    this.dialog=true;
+        computed: {
+            testdatum() {
+                if (!this.data.datum) {
+                    return "Kein Datum angegeben";
                 }
-                else this.dialog=false
+
+                return moment(this.data.datum).locale("de").format("L");
             }
-    }
+        },
+        methods: {
+            toogleDialog() {
+                if (this.dialog === false) {
+                    this.dialog = true;
+                } else this.dialog = false
+            }
+        }
     }
 </script>
 
