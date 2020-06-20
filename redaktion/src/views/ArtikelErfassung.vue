cab11c233d968c0b2149e2bf06bd20ad23ff76c8
Octo42 <anika.protz@gmail.com>
2020-06-20 22:49:43 +0200

    ArtikelErfassung responsive gestaltet, Erstellungszeit wird als ISOString geliefert

--- redaktion/src/views/ArtikelErfassung.vue
+++ redaktion/src/views/ArtikelErfassung.vue
@@ -1,48 +1,52 @@
 <template>
-    <v-container>
-        <v-form v-model="isValid" ref="form">
-            <v-text-field
-                    v-model="content.link"
-                    :rules="linkRules"
-                    label="Link zum Video/Artikel"
-                    required
-            />
-            <v-item-group v-model="isValid" :mandatory="true">
-                <v-checkbox
-                        hide-details
-                        v-model="selectedCheckboxen"
-                        :rules="checkboxRules "
-                        label="DGS"
-                        value="DGS"
+    <v-container fluid class="fill-height">
+        <v-row justify="center">
+            <v-col cols="11" md="4">
+                <v-form v-model="isValid" ref="form">
+                            <v-text-field
+                                    v-model="content.link"
+                                    :rules="linkRules"
+                                    label="Link zum Artikel/Video"
+                                    required
+                            />
+                    <v-item-group v-model="isValid" :mandatory="true">
+                        <v-checkbox
+                                hide-details
+                                v-model="selectedCheckboxen"
+                                :rules="checkboxRules "
+                                label="DGS"
+                                value="DGS"
 
-                />
-                <v-checkbox
-                        hide-details
-                        v-model="selectedCheckboxen"
-                        :rules="checkboxRules"
-                        label="Untertitel"
-                        value="Untertitel"
+                        />
+                        <v-checkbox
+                                hide-details
+                                v-model="selectedCheckboxen"
+                                :rules="checkboxRules"
+                                label="Untertitel"
+                                value="Untertitel"
 
-                />
-                <v-checkbox
-                        v-model="selectedCheckboxen"
-                        :rules="checkboxRules"
-                        label="Leichte/Einfache Sprache"
-                        value="Leichte/Einfache Sprache"
-                />
-            </v-item-group>
-
-            <v-btn @click="inhaltAnlegen()" :disabled="!isValid">
-                Senden
-            </v-btn>
-            <v-spacer></v-spacer>
-            <Alert v-if="success" type="success" :timeout="timeoutDuration" class="mb-4 mt-4">
-                News erfolgreich erfasst.
-            </Alert>
-            <Alert v-if="error" type="error" class="mb-4 mt-4">
-                Es ist ein Fehler aufgetreten.
-            </Alert>
-        </v-form>
+                        />
+                        <v-checkbox
+                                v-model="selectedCheckboxen"
+                                :rules="checkboxRules"
+                                label="Leichte/Einfache Sprache"
+                                value="Leichte/Einfache Sprache"
+                        />
+                    </v-item-group>
+                    <v-row justify="end" class="ma-0">
+                        <v-btn color="primary" @click="inhaltAnlegen()" :disabled="!isValid">
+                            Senden
+                        </v-btn>
+                    </v-row>
+                    <Alert v-if="success" type="success" :timeout="timeoutDuration" class="mb-4 mt-4">
+                        Artikel/Video erfolgreich erfasst.
+                    </Alert>
+                    <Alert v-if="error" type="error" class="mb-4 mt-4">
+                        Es ist ein Fehler aufgetreten.
+                    </Alert>
+                </v-form>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -58,7 +62,7 @@
                     "gebaerdensprachVideo": false,
                     "untertitel": false,
                     "leichteSprache": false,
-                    "link": null,
+                    "link": "",
                     "erfassungsZeit": null
                 },
                 selectedCheckboxen: [],
@@ -66,21 +70,13 @@
                 isValid: false,
                 error: null,
                 success: null,
-                linkRules: [v => /^(https:|http:|www\.)\S*/.test(v) || "Bitte geben Sie eine gültige URL ein."],
+                linkRules: [v => /^(https:|http:|www\.)\S*/.test(v) || "Bitte geben Sie eine gültige URL ein.", v => v.length <= 2048 || "Die URL ist zu lang."],
                 checkboxRules: [selectedCheckboxen => selectedCheckboxen.length > 0 || "Bitte wählen Sie mindestens eine der Optionen aus. "]
             }
         },
         methods: {
             ermittleDatum() {
-                //Zeitformat abklaeren yyyy-dd-mm oder yyyy-mm-dd
-                var today = new Date();
-                console.log(today);
-                var dd = today.getDate();
-                var mm = today.getMonth() + 1;
-                var yyyy = today.getFullYear();
-                if (dd < 10) dd = '0' + dd;
-                if (mm < 10) mm = '0' + mm;
-                this.content.erfassungsZeit = yyyy + "-" + dd + "-" + mm
+                var today = new Date().toDateString();
             },
             fuelleContentBools() {
                 this.content.gebaerdensprachVideo = this.selectedCheckboxen.includes('DGS');
